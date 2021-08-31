6bcdbd058579537216a88f80c0893b428328ee6a
NurFortuna <89857618+NurFortuna@users.noreply.github.com>
2021-08-31 18:12:28 +0300

    first commit

---
+++ app/src/main/java/com/fortuna/flowers/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.fortuna.flowers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+    public void changeActivity(View view) {
+
+        Intent intent = new Intent(this,MainActivity.class);
+
+        startActivity(intent);
+
+    }
+}
